2cd195ae1908b9e02712ebd2f1c64f52d4f830c5
HC008 <h.wk.cheng@gmail.com>
2014-07-31 16:58:48 -1000

    Change file chooser and and variable
    Changed a variable name to be filePath instead of fileName, and also
    changed how the window looks like for the JFileChooser.

--- src/Processor.java
+++ src/Processor.java
@@ -21,14 +21,14 @@ public class Processor {
    * It reads in all of the data from the CSV file.
    * 
    * @param data - String array list.
-   * @param fileName - name of file to be read in.
+   * @param filePath - path of file to be read in.
    * @return tweets - the filled up list of records.
    */
-  public List<TweetRecord> readCsv(List<TweetRecord> tweets, String fileName) {
+  public List<TweetRecord> readCsv(List<TweetRecord> tweets, String filePath) {
    
     try {
 
-      CSVReader file = new CSVReader(new FileReader(fileName));
+      CSVReader file = new CSVReader(new FileReader(filePath));
       List<String[]> data = new ArrayList<String[]>();
       String [] nextLine;
       int lineNumber = 3;
@@ -37,6 +37,7 @@ public class Processor {
         data.add(nextLine);
       }
       
+      //Creating the object while reading the lines of data from the file
       while (lineNumber >= 3 && lineNumber < data.size() - 2) {
         tweets.add(new TweetRecord(data.get(lineNumber)[0], data.get(lineNumber)[1], 
                                    data.get(lineNumber)[2], data.get(lineNumber)[3],
@@ -74,7 +75,7 @@ public class Processor {
     int small = 0, large = 0;
     List<TweetRecord> nonDuplicate = new ArrayList<TweetRecord>();
     
-    //Test which file size is smaller than the other and then compare it in two separate loops
+    //Test which file size is smaller than the other and then call a method to check for duplicate
     if (csvOne.size() < csvTwo.size() || csvOne.size() == csvTwo.size()) {
       //First file is smaller than second file
       small = csvOne.size();
@@ -119,12 +120,12 @@ public class Processor {
     } //end of outer for loop
     
     
-    //Loading all data into filtered list
+    //Loading all of the data into  the list
     filtered = less;
     
-    for (int m = 0; m < little; m++) {
-      if (less.get(m).getFlag() == 0) {
-        filtered.add(less.get(m));
+    for (int m = 0; m < big; m++) {
+      if (more.get(m).getFlag() == 0) {
+        filtered.add(more.get(m));
       }
     }
     
--- src/TweetGUI.java
+++ src/TweetGUI.java
@@ -2,6 +2,9 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -22,8 +25,13 @@ public class TweetGUI extends JApplet implements ActionListener {
   private JLabel fileOne = new JLabel("File One"), fileTwo = new JLabel("File Two");
   private JButton submit = new JButton("Submit"), browse = new JButton("Browse");
   private JTextField textOne = new JTextField(30), textTwo = new JTextField(30);
-  private JTextArea area = new JTextArea(40, 50);
+  private JTextArea area = new JTextArea(40, 100);
   private JScrollPane scroll;
+  private List<TweetRecord> dataOne = new ArrayList<TweetRecord>();
+  private List<TweetRecord> dataTwo = new ArrayList<TweetRecord>();
+  private List<TweetRecord> allData = new ArrayList<TweetRecord>();
+  private Processor process = new Processor();
+  private File[] files;
   
   public void init() {
     //Adding content to the app by adding things to panel etc
@@ -35,12 +43,14 @@ public class TweetGUI extends JApplet implements ActionListener {
     mainPanel.add(textTwo);
     mainPanel.add(browse);
     mainPanel.add(submit);
+    
+    //Prevents anyone from editing the area of the printed results.
+    area.setEditable(false);
     mainPanel.add(area);
     
     //Adds a scoll bar to the text area
     scroll = new JScrollPane(area, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, 
-                                      ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-    
+                                      ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     
     mainPanel.add(scroll);
     
@@ -58,11 +68,34 @@ public class TweetGUI extends JApplet implements ActionListener {
       
       //Opens up a window to allow users to select a file
       JFileChooser fileSelect = new JFileChooser();
-      int selection = fileSelect.showOpenDialog(this);
+      fileSelect.setMultiSelectionEnabled(true);
+      int selection = fileSelect.showDialog(TweetGUI.this, "Select");
+      
+      if (selection == JFileChooser.APPROVE_OPTION) {
+        files = fileSelect.getSelectedFiles();
+        
+        //Sets both text fields to be empty first to ensure that the previous selection is gone
+        //if the user decides to change selection. Then load the name of the file to the textfield.
+        textOne.setText("");
+        textOne.setText(files[0].getName());
+        
+        textTwo.setText("");
+        textTwo.setText(files[1].getName());
+      }
       
     }
     else if (e.getSource().equals(submit)) {
-      area.append("Done \n");
+      
+      dataOne = process.readCsv(dataOne, files[0].getPath());
+      dataTwo = process.readCsv(dataTwo, files[1].getPath());
+      
+      allData = process.checkFileSize(dataOne, dataTwo);
+      
+      int lineNumber = 1;
+      for (int i = 0; i < allData.size(); i++) {
+        area.append(lineNumber + ". " + allData.get(i).toString() + "\n\n");
+        lineNumber++;
+      }
     }
     
     
