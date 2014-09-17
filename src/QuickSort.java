775ab349e0861aa0a3f94c0c0f045efd65cea881
HC008 <h.wk.cheng@gmail.com>
2014-09-16 23:33:40 -1000

    Change QuickSort to sort by dates
    Changed the QuickSort class to actually quick sort a list of TweetRecord
    objects by comparing the dates of each object (which represents each
    line of data from the CSV file).

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,53 +1,66 @@
+import java.util.List;
+
 /**
- * This QuickSort class is primarily used to sort a given array of numbers through the driver class.  The quicksort
- * method will call the partition method if the first index is less than the last.  Otherwise, a recursive quicksort
- * call will be made from the first element to the pivot index minus one, as well as another recursive quicksort call
- * from the pivot index plus one all the way until the last element of the array.  If only an array is given, 
- * the left endpoint is the first element of the array, and the right endpoint is the last element of the array. 
+ * This QuickSort class is primarily used to sort a given list of TweetRecord objects. 
+ * The quicksort method will call the partition method if the first index 
+ * is less than the last.  Otherwise, a recursive quicksort call will be made from the first 
+ * element to the pivot index minus one, as well as another recursive quicksort call
+ * from the pivot index plus one all the way until the last element of the list.  
+ * If only an list is given, the left endpoint is the first element of the list, 
+ * and the right endpoint is the last element of the list. 
  * 
  * @author Original Author:  Jason Delos Reyes
- * @author Derivative Authors:  Hansen Cheng, Micah Angeles, Jason Delos Reyes	
+ * @author Derivative Authors:  Hansen Cheng, Micah Angeles, Jason Delos Reyes  
  */
 public class QuickSort {
 	
 	protected int comparison = 0;
+	private List<TweetRecord> records;
 	
-	/**A constructor method to allow the driver to utilize this QuickSort class.
-	 */
-	public QuickSort(){	
+	/**
+   * Constructs the object to contain the unsorted list of records.
+   * 
+   * @param records - unsorted list of TweetRecord objects.
+   */
+	public QuickSort(List<TweetRecord> records){
+	  this.records = records;
 	}
 	
 	
-	/**If only an array is given as a parameter, then the quicksort method will be overrided to have the left index
-	 * as the first element of the array, while the right index as the last element of the array.
+	/**If only an list is given as a parameter, then the quicksort method will 
+	 * be overrided to have the left index as the first element of the list, 
+	 * while the right index as the last element of the list.
 	 */
-	public void quickSort(int[] array) {
-		quickSort(array, 0, array.length -1);
+	public void quickSort(List<TweetRecord> record) {
+		quickSort(record, 0, record.size() - 1);
 	}
 	
-	/** If the first element of the array is greater than the last element of the array, the partition method is called.
+	/** If the first element of the list is greater 
+	 * than the last element of the list, the partition method is called.
 	 */
-	public void quickSort(int[] array, int first, int last) {
+	public void quickSort(List<TweetRecord> record, int first, int last) {
 		
 		if (first < last) {
-			int pivIndex = partition(array, first, last);
+			int pivIndex = partition(record, first, last);
 			
-			quickSort(array, first, pivIndex - 1);
+			quickSort(record, first, pivIndex - 1);
 			
-			quickSort(array, pivIndex + 1, last);
+			quickSort(record, pivIndex + 1, last);
 		}
 		
 	}
 	
-	/** The method contains three variables:  a pivot value, a left pointer, and a right pointer.  The program
-	 * will find the location where the pivot value is less than, or equal to the value of the leftPointer as well 
-	 * as finding where the pivot value is less than the rightPointer value.  When both values have been found, if
-	 * the left pointer is less than the right pointer, then they will swap values.  The rightPointer value will be
-	 * returned to determine which subarray needs to be quick-sorted.
+	/** The method contains three variables:  a pivot value, a left pointer, and a 
+	 * right pointer. The program will find the location where the pivot value is 
+	 * less than, or equal to the value of the leftPointer as well as finding where 
+	 * the pivot value is less than the rightPointer value.  When both values have 
+	 * been found, if the left pointer is less than the right pointer, then they 
+	 * will swap values.  The rightPointer value will be returned to determine which sublist 
+	 * needs to be quick-sorted.
 	 */
-	public int partition(int[] array, int first, int last) {
+	public int partition(List<TweetRecord> record, int first, int last) {
 		
-		int pivot = array[first];
+		String pivot = record.get(first).getTweetDate();
 		int leftPointer = first;
 		int rightPointer = last;
 		
@@ -58,29 +71,34 @@ public class QuickSort {
 			//Finds the point where either leftPointer is more than last (rightmost index) OR the pivot is less
 			//than the element at array[leftPointer].  If not, the leftPointer index increases until either condition
 			//is met.
-			while ((leftPointer < last) && (pivot >= array[leftPointer])) {
-				leftPointer++;
+			while ((leftPointer < last) && (pivot.compareTo(record.get(leftPointer).getTweetDate()) >= 
+			       record.get(leftPointer).getTweetDate().compareTo(pivot))) {
+				
+			  leftPointer++;
 			}
 			
 			//Finds the point where the pivot is equal to or greater than the element at array[rightPointer].  
 			//Otherwise, the rightPointer index increases until the condition is met.
-			while (pivot <  array[rightPointer]) {
-				rightPointer--;
+			while (pivot.compareTo(record.get(rightPointer).getTweetDate()) < 
+			        record.get(rightPointer).getTweetDate().compareTo(pivot)) {
+				
+			  rightPointer--;
 			}
 			
-			//At this point, if the leftPointer is less than the rightPointer, the elements of array[leftPointer]
-			//and array[rightPointer] will swap.
+			//At this point, if the leftPointer is less than the rightPointer, the elements of 
+			//record.get(leftPointer)and record.get(rightPointer) will be swapped.
 			if (leftPointer < rightPointer) {
-				int temp = array[leftPointer];
-				array[leftPointer] = array[rightPointer];
-				array[rightPointer] = temp;
+				TweetRecord temp = record.get(leftPointer);
+				record.set(leftPointer, record.get(rightPointer));
+				record.set(rightPointer, temp);
 				comparison++;
 			}
 		}
-			//After the large while loop, the elements at array[rightPointer] and array[first] will swap
-			int temp = array[rightPointer];
-			array[rightPointer] = array[first];
-			array[first] = temp;
+			//After the large while loop, the elements at 
+		  //record.get(rightPointer) and record.get(first) will be swapped.
+			TweetRecord temp = record.get(rightPointer);
+			record.set(rightPointer, record.get(first));
+			record.set(first, temp);
 			comparison++;
 			
 			//The right index is returned to be used to determine the next partition.
@@ -89,7 +107,12 @@ public class QuickSort {
 		
 	}
 	
-	
-	
-	
+	/**
+	 * Retrieves the sorted list of TweetRecord objects.
+	 * 
+	 * @return records - list of TweetRecord objects.
+	 */
+	public List<TweetRecord> getRecord() {
+	  return records;
+	}
 }
